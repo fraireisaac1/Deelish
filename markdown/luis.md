ef85d502b5620ed786898f24d6d9d3026dde0c16
Luis Torres <ltorre701@west-mec.org>
2024-10-01 17:31:13 -0700

    updated my journal and the put route

--- markdown/luis.md
+++ markdown/luis.md
@@ -1,3 +1,13 @@
 # Hello
-
 Hi from Luis
+
+# Journal
+## 10/1/2024
+1. What did you accomplish today?
+> Today I got access to the project and the repo. I added the JSON files and began to write the code for updating the username, email, and password of users.
+2. What is your role and responsibilites?
+> My role is Backend Specialist. I'm responsible for most of the JavaScript code, with the put, delete, post, and get with try catch.
+3. What do you hope to accomplish tomorrow?
+> Tomorrow I hope to complete the put route to update the user and finish the delete route. 
+4. What was your biggest challenge today?
+> The biggest challenge was just getting the repo set up and making sure everyone can access the files.
--- server/index.js
+++ server/index.js
@@ -14,4 +14,28 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: clientPath });
- });
+});
+ 
+app.put('/update user/:currentName/:currentEmail/:currentPassword', async (req, res) => {
+    try {
+        const { currentName, currentEmail, currentPassword } = req.params;
+        const { newName, newEmail, newPassword } = req.body;
+        console.log('Current user:', { currentName, currentEmail });
+        console.log('New user data:', { newName, newEmail, newPassword });
+        const data = await fs.readFile(dataPath, 'utf8');
+        if (data) {
+            let users = JSON.parse(data);
+            const userIndex = users.findIndex(user => user.name === currentName && user.email === currentEmail && user.password === currentPassword);
+            if (userIndex === -1) {
+                return res.status(404).json({ message: "User not found" });
+            }
+            users[userIndex] = { ...users[userIndex], name: newName, email: newEmail };
+            console.log(users);
+            await fs.writeFile(dataPath, JSON.stringify(users, null, 2));
+            res.status(200).json({ message: `You sent ${newName}, ${newEmail} and ${newPassword}` });
+        }
+    }
+    catch (error) {
+        
+     }
+})

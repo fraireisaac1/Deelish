e1e60111c455978f882ff185457d74515bc5d0e6
Luis Torres <ltorre701@west-mec.org>
2024-10-02 16:26:54 -0700

    completed the post, put and delete routes

--- markdown/luis.md
+++ markdown/luis.md
@@ -6,7 +6,7 @@ Hi from Luis
 1. What did you accomplish today?
 > Today I got access to the project and the repo. I added the JSON files and began to write the code for updating the username, email, and password of users.
 2. What is your role and responsibilites?
-> My role is Backend Specialist. I'm responsible for most of the JavaScript code, with the put, delete, post, and get with try catch.
+> My role is Backend Specialist. I'm responsible for most of the JavaScript code, with the put, delete, post, and get routes utilizing try catch.
 3. What do you hope to accomplish tomorrow?
 > Tomorrow I hope to complete the put route to update the user and finish the delete route. 
 4. What was your biggest challenge today?
--- server/index.js
+++ server/index.js
@@ -4,9 +4,10 @@ const fs = require('fs').promises;
 
 const app = express();
 
+const serverPublic = path.join(__dirname, 'public');
+
 const clientPath = path.join(__dirname, "..", "client/src");
-const usersPath = path.join(__dirname, "data", "users.json");
-const recipesPath = path.join(__dirname, "data", "recipes.json");
+const dataPath = path.join(__dirname, 'data', 'users.json', 'recipes.json');
 
 app.use(express.static(clientPath));
 app.use(express.urlencoded({ extended: true }));
@@ -15,7 +16,53 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: clientPath });
 });
- 
+
+app.get('/form', (req, res) => {
+    res.sendFile('pages/form.html', { root: serverPublic });
+})
+
+app.get('/index', (req, res) => {
+    res.sendFile('pages/form.html', { root: serverPublic });
+})
+
+app.get('/users', async (req, res) => {
+    try {
+        const data = await fs.readFile(dataPath, 'utf8');
+
+        const users = JSON.parse(data);
+        if (!users) {
+            throw new Error("Hey that's not a user!");
+        }
+        res.status(200).json(users);
+    } catch (error) {
+        console.error("Problem getting users" + error.message);
+        res.status(500).json({ error: "Problem reading users" });
+    }
+});
+
+app.post('/submit-form', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        let users = [];
+        try {
+            const data = await fs.readFile(dataPath, 'utf8');
+            users = JSON.parse(data);
+        } catch (error) {
+            console.error('Error reading the user data:', error);
+            users = [];
+        }
+
+        let user = users.find(u => u.name === name && u.email == email && u.password === password);
+
+        await fs.writeFile(dataPath, JSON.stringify(users, null, 2));
+        res.redirect('form');
+    } catch (error) {
+        console.error('Error processing form:', error);
+        res.status(500).send('Oops, an error occured while processing your submission.');
+    }
+})
+
 app.put('/update user/:currentName/:currentEmail/:currentPassword', async (req, res) => {
     try {
         const { currentName, currentEmail, currentPassword } = req.params;
@@ -36,6 +83,35 @@ app.put('/update user/:currentName/:currentEmail/:currentPassword', async (req, 
         }
     }
     catch (error) {
-        
-     }
+        console.error('Error updating user:', error);
+        res.status(500).send('An error occured while trying to update the user account')
+    }
+});
+
+app.delete('/user/:name/:email/:password', async (req, res) => {
+    try {
+        const { name, email, password } = req.params;
+        let users = [];
+        try {
+            const data = await fs.readFile(dataPath, 'utf8');
+            users.JSON(parse(data));
+        } catch (error) {
+            return res.status(404).send(`File data can't be found`);
+        }
+        const userIndex = users.findIndex(user => user.name === name && user.email === email && user.password === password);
+        if (userIndex === -1) {
+            return res.status(404).json({ message: 'User was not found :(' });
+        }
+        users.splice(userIndex, 1);
+        console.log(userIndex);
+        console.log(users);
+        try {
+            await fs.writeFile(dataPath, JSON.stringify(users, null, 2));
+        } catch (error) {
+            res.status(500).send("There was a problem!!!");
+        }
+        return res.send('Nice you deleted user');
+    } catch (error) {
+        console.error("there was an error");
+    }
 })

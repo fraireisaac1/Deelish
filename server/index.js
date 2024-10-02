1ee71a442c571511bf71ac801cfddd5dcf21abdc
Isaac Fraire <ifrair729@west-mec.org>
2024-10-01 17:34:13 -0700

    added my changes

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const pash = require('path');
+const fs = require('fs').promises;
+
+const app = express();
+
+const clientPath = path.join(__dirname, "..", "client/src");
+const usersPath = path.join(__dirname, "data", "users.json");
+const recipesPath = path.join(__dirname, "data", "recipes.json");
+
+app.use(express.static(clientPath));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: clientPath });
+ });
+
+app.post('/submit-form', async (req, res) => { 
+    try {
+        const { name, email, password } = req.body;
+        let users = [];
+    } catch(error) {}
+});
